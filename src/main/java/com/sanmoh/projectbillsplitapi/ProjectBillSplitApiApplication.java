cb02bbab467638c3c9e19d1073ed14e520a2b057
Sanmoh Hombal <sanmoh.hombal@gmail.com>
2021-03-20 12:02:17 +0530

    Initial Commit

---
+++ src/main/java/com/sanmoh/projectbillsplitapi/ProjectBillSplitApiApplication.java
@@ -0,0 +1,13 @@
+package com.sanmoh.projectbillsplitapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBillSplitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBillSplitApiApplication.class, args);
+    }
+
+}
